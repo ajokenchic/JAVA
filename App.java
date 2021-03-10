5ea5b5c4dacf9c6ea12dfbe37ecffc385d37d362
ajokenchic <73014488+ajokenchic@users.noreply.github.com>
2021-03-10 20:24:10 +0900

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+//package com.tuyano.gradle;
+//
+//public class App {
+//    public static void main(String[] args) {
+//        App app = new App();
+//        System.out.println("===App class===");
+//        app.showMessage("Taro");
+//        app.showMessage("Hanako");
+//        System.out.println("APPCLASS");
+//    }
+//
+//    public boolean showMessage(String name) {
+//        try {
+//            System.out.println(this.getMessage(name));
+//        } catch (Exception e) {
+//            System.out.println("[ERR: " + e.getMessage() + "]");
+//            return false;
+//        } finally {
+//            return true;
+//        }
+//    }
+//
+//    public String getMessage(String name) throws Exception {
+//        if (name == null || name == "") {
+//            throw new Exception("no name!");
+//        }
+//        return "Hi," + name + ". welcome to gradle world!";
+//    }
+//}
+//
